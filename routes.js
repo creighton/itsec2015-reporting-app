70924d8c614ad74d459d2419bc2c45a77f8ae5f9
tom creighton <tjcreighton@gmail.com>
2015-11-17 13:06:54 -0500

    spinning off request parsing

--- routes.js
+++ routes.js
@@ -29,11 +29,15 @@ Router.route('/attempt/:_id', {
 Router.route('/xapi/webhook', {where: 'server'})
     .post(function() {
         try {
-            this.request.body.statements.forEach(function (cur, idx, arr){
-                if ( !cur.timestamp ) cur.timestamp = (new Date()).toISOString();
-                cur._timestamp = new Date(cur.timestamp);
-                Statements.insert(encodeKeys(cur));
-            });
+            var stmts = this.request.body.statements;
+            setTimeout(Meteor.bindEnvironment(function () {
+                if (! stmts) return;
+                stmts.forEach(function (cur, idx, arr){
+                    if ( !cur.timestamp ) cur.timestamp = (new Date()).toISOString();
+                    cur._timestamp = new Date(cur.timestamp);
+                    Statements.insert(encodeKeys(cur));
+                });
+            }), 1);
             this.response.writeHead(200, {"Content-Type":"text/plain"});
             this.response.write("200 OK");
             this.response.end();
