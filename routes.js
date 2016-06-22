60613e74c25507b4df9217d53214e0a5e6d09ef1
tom creighton <tjcreighton@gmail.com>
2016-06-22 08:50:23 -0400

    fixing route issue

--- routes.js
+++ routes.js
@@ -13,11 +13,12 @@ Router.route('/', {
 
 Router.route('/attempt/:_id', {
     template: 'infopane',
+    name: 'attempt',
     onBeforeAction: function () {
         try {
             var attstmt = Statements.findOne({_id: this.params._id});
             Session.set('attemptid', attstmt.object.id);
-        } catch (e) { 
+        } catch (e) {
             console.log('exception getting statement with id: ' + this.params._id);
             console.log(e);
             Session.set('attemptid', undefined);
@@ -28,14 +29,18 @@ Router.route('/attempt/:_id', {
 
 Router.route('/xapi/webhook', {where: 'server'})
     .post(function() {
+        console.log('here');
         try {
+            console.log("this request", this.request.body);
             var stmts = this.request.body.statements;
+            console.log('stmst', stmts);
             setTimeout(Meteor.bindEnvironment(function () {
                 if (! stmts) return;
                 stmts.forEach(function (cur, idx, arr){
                     if ( !cur.timestamp ) cur.timestamp = (new Date()).toISOString();
                     cur._timestamp = new Date(cur.timestamp);
-                    Statements.insert(encodeKeys(cur));
+                    var val = Statements.insert(encodeKeys(cur));
+                    console.log("here?", Statements.find({"_id": val}));
                 });
             }), 1);
             this.response.writeHead(200, {"Content-Type":"text/plain"});
@@ -46,5 +51,5 @@ Router.route('/xapi/webhook', {where: 'server'})
             this.response.write(e.message);
             this.response.end();
         }
-        
+
     });
